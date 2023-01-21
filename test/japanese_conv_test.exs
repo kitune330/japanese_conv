7103f643d808500757e56736059813a67979dfb4
kitune <kitune@mstdn.kitune330.com>
2023-01-21 15:03:43 +0900

    全半角変換

--- test/japanese_conv_test.exs
+++ test/japanese_conv_test.exs
@@ -2,7 +2,41 @@ defmodule JapaneseConvTest do
   use ExUnit.Case
   doctest JapaneseConv
 
-  test "greets the world" do
-    assert JapaneseConv.hello() == :world
+  test "full to half" do
+
+    result = JapaneseConv.full2Half("アイウエオ")
+    assert "ｱｲｳｴｵ" == result
+
+    result = JapaneseConv.full2Half("あイウエオ")
+    assert "あｲｳｴｵ" == result
+
+    result = JapaneseConv.full2Half("がギグげご")
+    assert "がｷﾞｸﾞげご" == result
+
+    result = JapaneseConv.full2Half("AＢｃデ５")
+    assert "ABcﾃﾞ5" == result
+  end
+
+
+  test "full to half numbers" do
+
+    result = JapaneseConv.full2HalfNum("アイウエオ")
+    assert "アイウエオ" == result
+
+    result = JapaneseConv.full2HalfNum("AＢｃデ５")
+    assert "AＢｃデ5" == result
+  end
+
+  test "half to full" do
+
+    result = JapaneseConv.half2Full("ｱｲｳｴｵ")
+    assert "アイウエオ" == result
+
+    result = JapaneseConv.half2Full("あｲｳｴｵ")
+    assert "あイウエオ" == result
+
+    result = JapaneseConv.half2Full("がｷﾞｸﾞげご")
+    assert "がギグげご" == result
+    IO.inspect(result)
   end
 end
